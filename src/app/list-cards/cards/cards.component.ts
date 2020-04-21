a6fbe3cf7e56c6452bedbe57199a5c8c88310efe
Helio gonzaga <helio.santosjunior@outlook.com>
2020-04-21 00:19:14 -0300

    criei mock profiles e populei componente card

--- src/app/list-cards/cards/cards.component.ts
+++ src/app/list-cards/cards/cards.component.ts
@@ -9,6 +9,47 @@ export class CardsComponent implements OnInit {
 
   constructor() { }
 
+  
+  profiles = [
+    {
+      id: "1",
+      name: "Mariana Petrecu",
+      area: "Mobile Designer",
+      contract: "Freelancer",
+      skills: ["React", "IOS", "React Native"],
+      description: "Lorem ipsum dolor in quam iure, odio amet atque.",
+      imageURL: "../../../../assets/images/avatar.jpg",
+      hour: "R$ 32/hr"
+    },{
+      id: "2",
+      name: "Mariana Petrecu",
+      area: "Mobile Designer",
+      contract: "Freelancer",
+      skills: ["React", "IOS", "React Native"],
+      description: "Lorem ipsum dolor in quam iure, odio amet atque.",
+      imageURL: "../../../../assets/images/avatar.jpg",
+      hour: "R$ 30/hr"
+    },{
+      id: "3",
+      name: "Mariana Petrecu",
+      area: "Mobile Designer",
+      contract: "Freelancer",
+      skills: ["React", "IOS", "React Native"],
+      description: "Lorem ipsum dolor in quam iure, odio amet atque.",
+      imageURL: "../../../../assets/images/avatar.jpg",
+      hour: "R$ 26/hr"
+    },{
+      id: "4",
+      name: "Mariana Petrecu",
+      area: "Mobile Designer",
+      contract: "Freelancer",
+      skills: ["React", "IOS", "React Native"],
+      description: "Lorem ipsum dolor in quam iure, odio amet atque.",
+      imageURL: "../../../../assets/images/avatar.jpg",
+      hour: "R$ 60/hr"
+    }
+  ]
+
   ngOnInit() {
   }
 
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -9,7 +9,7 @@ export class CardComponent implements OnInit {
 
   constructor() { }
 
-  image = '../../../../assets/images/avatar.jpg';
+   @Input() profile: any;
 
   ngOnInit() {
   }
