6db873420f4fb56c86e2183f03791b19ec68dffa
Helio gonzaga <helio.santosjunior@outlook.com>
2020-04-23 05:56:15 -0300

    criei o card do perfil e adicioneis 4 icones svgs

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -9,6 +9,24 @@ export class ProfileComponent implements OnInit {
 
   constructor() { }
 
+  image = "/assets/images/avatar.jpg";
+
+  networks = [
+    {
+      name: "linkedin",
+      svg: "/assets/svg/linkedin.svg"
+    },{
+      name: "facebook",
+      svg: "/assets/svg/facebook.svg"
+    },{
+      name: "twitter",
+      svg: "/assets/svg/twitter.svg"
+    },{
+      name: "github",
+      svg: "/assets/svg/github.svg"
+    }
+  ]
+
   ngOnInit() {
   }
 
