7c470f4b040e5f28cdb26ad0975c2fee210bca07
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-05-20 20:51:19 -0300

    mudei para a classe para const

--- src/app/shared/mocks/networks.mock.ts
+++ src/app/shared/mocks/networks.mock.ts
@@ -1,15 +1,6 @@
-export class NetworksMock {
-
-    constructor(){}
-
-    networks = [
-        {linkedin: "/assets/svg/linkedin.svg"},
-        {facebook: "/assets/svg/facebook.svg"},
-        {twitter: "/assets/svg/twitter.svg"},
-        {github: "/assets/svg/github.svg"}
-    ]
-    
-    get getNetworks(): Object {
-        return this.networks;
-    }
-}
+export const NetworksMock = [
+    {linkedin: "/assets/svg/linkedin.svg"},
+    {facebook: "/assets/svg/facebook.svg"},
+    {twitter: "/assets/svg/twitter.svg"},
+    {github: "/assets/svg/github.svg"}
+]
