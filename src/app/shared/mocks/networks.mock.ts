1329c4bc49d8ec293b7b9cf6626ebfadefab9539
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-05-20 22:58:09 -0300

    refatorei mock de icones de rede

--- src/app/shared/mocks/networks.mock.ts
+++ src/app/shared/mocks/networks.mock.ts
@@ -1,6 +1,8 @@
-export const NetworksMock = [
-    {linkedin: "/assets/svg/linkedin.svg"},
-    {facebook: "/assets/svg/facebook.svg"},
-    {twitter: "/assets/svg/twitter.svg"},
-    {github: "/assets/svg/github.svg"}
-]
+import { MockNeteworks } from './../models/mock.model';
+
+export const NetworksMock: MockNeteworks = {
+    linkedin: "/assets/svg/linkedin.svg",
+    facebook: "/assets/svg/facebook.svg",
+    twitter: "/assets/svg/twitter.svg",
+    github: "/assets/svg/github.svg"
+}
