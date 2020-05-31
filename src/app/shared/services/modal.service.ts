96874cf94feaf8b55b979c00be93b746dde2cf3d
Helio Gonzaga <helio.santosjunior@outlook.com>
2020-05-31 19:51:37 -0300

    criei metodo filterProfiles

--- src/app/shared/services/modal.service.ts
+++ src/app/shared/services/modal.service.ts
@@ -1,13 +1,19 @@
 import { EventEmitter, Injectable } from '@angular/core';
 
+import { FilterModal } from './../models/filter-modal.module';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  constructor() {}
-    
   modalSearch = new EventEmitter<boolean>();
-  
+  filterProfile = new EventEmitter<FilterModal>();
+
+  constructor() {}
+      
   openModalSearch = (value: boolean) => this.modalSearch.emit(value);
+
+  filterProfiles = (profiles: FilterModal) => this.filterProfile.emit(profiles);
+
 }
